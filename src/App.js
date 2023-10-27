fa273086696e87dd0a7910d248d2e68683940d58
Himanshu Sanga <sanhimanshusanga51019@gmail.com>
2023-10-27 17:08:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
             <div className="mainbtn">
               <button className="mybtn resumebtn">
                 <img src={require('./resumepng.png')} alt="" />
-                <a href={require('./RESUME-HS.pdf')} download>Download Resume</a>
+                <a href={require('./RESUME-hs.pdf')} download>Download Resume</a>
               </button>
               <button className="mybtn">
                 <img src={require('./linkedin.png')} alt="" />
